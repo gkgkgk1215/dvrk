15077dd03e2bc8c83d25f56938f5febfff5d6c92
hwangmh <gkgk1215@gmail.com>
2019-09-18 19:09:58 -0700

    opencv

--- OpenCV.py
+++ OpenCV.py
@@ -395,6 +395,60 @@ def thresholding():
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+"""
+Image blur
+"""
+
+# 2D convolution (image filtering)
+def bluring_filtering():
+    img = cv2.imread('img/left_image_raw.png')
+
+    kernel = np.ones((5,5), np.float32)/25
+    blur = cv2.filter2D(img, -1, kernel)
+
+    cv2.imshow('original', img)
+    cv2.imshow('blur', blur)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def onMouse(x):
+    pass
+
+def bluring():
+    img = cv2.imread('img/left_image_raw.png')
+
+    cv2.namedWindow('BlurPane')
+    cv2.createTrackbar('BLUR_MODE', 'BlurPane', 0, 2, OnMouse)
+    cv2.createTrackbar('BLUR', 'BlurPane', 0, 5, OnMouse)
+
+    mode = cv2.getTrackbarPos('BLUR_MODE', 'BlurPane')
+    val = cv2.getTrackbarPos('BLUR', 'BlurPane')
+
+    while True:
+        val = val*2 + 1
+        try:
+            if mode==0:
+                blur = cv2.blur(img, (val, val))
+            elif mode==1:
+                blur = cv2.GaussianBlur(img, (val,val), 0)
+            elif mode==2:
+                blur = cv2.medianBlur(img, val)
+            else:
+                break
+            cv2.imshow('BlurPane', blur)
+        except:
+            break
+
+        key = cv2.waitKey(1) & 0xFF
+        if key==27:
+            break
+
+        mode = cv2.getTrackbarPos('BLUR_MODE', 'BlurPane')
+        val = cv2.getTrackbarPos('BLUR', 'BlurPane')
+
+    cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
     # showImage()
@@ -410,4 +464,6 @@ if __name__ == "__main__":
     # bitOperation(800,10)
     # hsv()
     # color_tracking()
-    thresholding()
+    # thresholding()
+    # bluring_filtering()
+    bluring()
