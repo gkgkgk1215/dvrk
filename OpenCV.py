6f11aec24dfe02db54e37b9cb3b82279d783cdcb
hwangmh <gkgk1215@gmail.com>
2019-09-26 05:58:23 -0700

    opencv updated

--- OpenCV.py
+++ OpenCV.py
@@ -1,7 +1,7 @@
 import cv2
 import matplotlib.pyplot as plt
-import numpy as np
 from random import shuffle
+import numpy as np
 
 """
 Image showing
@@ -449,6 +449,280 @@ def bluring():
 
     cv2.destroyAllWindows()
 
+"""
+image contour
+"""
+def moment():
+    img = cv2.imread('img/berkeley_logo.png')
+    img1 = img.copy()
+    imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    threshold = 127
+    ret, thr = cv2.threshold(imgray, threshold, 255, 0)
+    ret, thr1 = cv2.threshold(imgray, threshold, 255, cv2.THRESH_BINARY)
+    ret, thr2 = cv2.threshold(imgray, threshold, 255, cv2.THRESH_BINARY_INV)
+    ret, thr3 = cv2.threshold(imgray, threshold, 255, cv2.THRESH_TRUNC)
+    ret, thr4 = cv2.threshold(imgray, threshold, 255, cv2.THRESH_TOZERO)
+    ret, thr5 = cv2.threshold(imgray, threshold, 255, cv2.THRESH_TOZERO_INV)
+
+    contours, _ = cv2.findContours(thr, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cnt = contours[40]
+    mmt = cv2.moments(cnt)
+    for key, val in mmt.items():
+        print ('%s:\t%.5f' %(key,val))
+
+    # center of moment
+    cx = int(mmt['m10']/mmt['m00'])
+    cy = int(mmt['m01']/mmt['m00'])
+    print(cx, cy)
+
+    # area & perimeter
+    area = cv2.contourArea(cnt)
+    perimeter = cv2.arcLength(cnt, True)
+    print ('Area =', area)
+    print ('Perimeter = ', perimeter)
+
+    for i in range(len(contours)):
+        epsilon = 0.01*cv2.arcLength(cnt, True)
+        approx_PD = cv2.approxPolyDP(contours[i], epsilon, True)
+        cv2.drawContours(img, [approx_PD], 0, (255,255,0), 1)
+        cv2.imshow('contour', img)
+
+        check = cv2.isContourConvex(contours[i])
+        if not check:
+            hull = cv2.convexHull(contours[i])
+            cv2.drawContours(img1, [hull], 0, (255,255,0), 1)
+        cv2.imshow('convex_hull', img1)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def convex():
+    img = cv2.imread('img/hand.jpg')
+    img1 = img.copy()
+    imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    ret, thr = cv2.threshold(imgray, 127, 255, 0)
+    contours, _ = cv2.findContours(thr, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    cnt = contours[1]
+    cv2.drawContours(img, [cnt], 0, (0, 255, 0), 3)
+    cv2.imshow('contour', img)
+
+    check = cv2.isContourConvex(cnt)
+    if not check:
+        hull = cv2.convexHull(cnt)
+        cv2.drawContours(img1, [hull], 0, (0, 255, 0), 3)
+        cv2.imshow('convex_hull', img1)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def contour():
+    img = cv2.imread('img/crash.jpg')
+    imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    rows, cols = img.shape[:2]
+
+    ret, thr = cv2.threshold(imgray, 127, 255, 0)
+    contours, hierarchy = cv2.findContours(thr, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # contour hierarchy
+
+    cnt = contours[5]
+
+    # bounding rectangle
+    x,y,w,h = cv2.boundingRect(cnt)
+    cv2.rectangle(img, (x,y), (x+w,y+h), (0, 0, 255), 3)
+
+    # minimum area rectangle
+    rect = cv2.minAreaRect(cnt)
+    box = cv2.boxPoints(rect)
+    box = np.int0(box)
+    print (rect)
+    print (box)
+
+    cv2.drawContours(img, [box], 0, (0, 255, 0), 3)
+
+    # minimum enclosing circle
+    (x,y), r = cv2.minEnclosingCircle(cnt)
+    center = (int(x), int(y))
+    radius = int(r)
+    cv2.circle(img, center, radius, (255, 0, 0), 3)
+
+    # fitting ellipse
+    ellipse = cv2.fitEllipse(cnt)
+    cv2.ellipse(img, ellipse, (0,255,0), 3)
+
+    # fitting line
+    [vx, vy, x, y] = cv2.fitLine(cnt, cv2.DIST_L2, 0, 0.01, 0.01)
+    ly = int((-x*vy/vx)+y)
+    ry = int(((cols-x)*vy/vx)+y)
+    cv2.line(img, (cols-1, ry), (0, ly), (0, 0, 255), 3)
+    cv2.imshow('contour', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def contour_adv():
+    img = cv2.imread('img/crash.jpg')
+    imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, thr = cv2.threshold(imgray, 127, 255, 0)
+    contours, _ = cv2.findContours(thr, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cnt = contours[5]
+
+    hull = cv2.convexHull(cnt)
+    cv2.drawContours(img, [hull], 0, (0, 0, 255), 2)
+
+    # defects calculation
+    hull = cv2.convexHull(cnt, returnPoints=False)
+    defects = cv2.convexityDefects(cnt, hull)
+
+    for i in range(defects.shape[0]):
+        sp, ep, fp, dist = defects[i, 0]
+        start = tuple(cnt[sp][0])
+        end = tuple(cnt[ep][0])
+        farthest = tuple(cnt[fp][0])
+        cv2.circle(img, farthest, 5, (0, 255, 0), -1)
+
+
+    # distance to contour
+    outside = (55,150)
+    inside = (140, 100)
+
+    dist1 = cv2.pointPolygonTest(cnt, outside, True)
+    dist2 = cv2.pointPolygonTest(cnt, inside, True)
+
+    print ('Distance from contour to point (%d, %d) = %.3f' %(outside[0], outside[1], dist1))
+    print ('Distance from contour to point (%d, %d) = %.3f' %(inside[0], inside[1], dist2))
+
+    cv2.circle(img, outside, 3, (0, 255, 0), -1)
+    cv2.circle(img, inside, 3, (255, 0, 255), -1)
+
+    cv2.imshow('defects', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    # cv2.matchShapes()
+
+def histogram():
+    pass
+
+# image histrogram equalization
+# CLAHE (Contrast Limited Adaptive Histogram Equalization)
+def equalization():
+    pass
+
+# 2D image histogram
+def hist2D():
+    pass
+
+# image histrogram background projection
+def backProjection():
+    pass
+
+# fourier transform
+def fourier():
+    pass
+
+# template matching
+def tmpMatching():
+    pass
+
+# line/circle detection using the Hough transformation
+def hough():
+    pass
+
+# image segmentation using watershed
+def watershed():
+    pass
+
+# foreground abstraction using grabcut algorithm
+def grabcut():
+    pass
+
+
+# object tracking
+col, width, row, height = -1, -1, -1, -1
+frame = None
+frame2 = None
+inputmode = False
+rectangle = False
+trackWindow = None
+roi_hist = None
+
+def onMouse(event, x, y, flags, param):
+    global col, width, row, height, frame, frame2, inputmode
+    global rectangle, roi_hist, trackWindow
+
+    if inputmode:
+        if event == cv2.EVENT_LBUTTONDOWN:
+            rectangle = True
+            col, row = x, y
+
+        elif event == cv2.EVENT_MOUSEMOVE:
+            if rectangle:
+                frame = frame2.copy()
+                cv2.rectangle(frame, (col,row), (x,y), (0,255,0), 2)
+                cv2.imshow('frame', frame)
+
+        elif event == cv2.EVENT_LBUTTONUP:
+            inputmode = False
+            rectangle = False
+            cv2.rectangle(frame, (col,row), (x,y), (0,255,0), 2)
+            height, width = abs(row-y), abs(col-x)
+            trackWindow = (col, row, width, height)
+            roi = frame[row:row+height, col:col+width]
+            roi_hist = cv2.calcHist([roi], [0], None, [180], [0,180])
+            cv2.normalize(roi_hist, roi_hist, 0, 255, cv2.NORM_MINMAX)
+
+    return
+
+def camShift():
+    global frame, frame2, inputmode, trackWindow, roi_hist, out
+    try:
+        cap = cv2.VideoCapture(0)
+        cap.set(3, 480)
+        cap.set(4, 320)
+    except:
+        print('camera operation failed')
+        return
+
+    ret, frame = cap.read()
+
+    cv2.namedWindow('frame')
+    cv2.setMouseCallback('frame', onMouse, param=(frame, frame2))
+
+    termination = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1)
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        if trackWindow is not None:
+            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            dst = cv2.calcBackProject([hsv], [0], roi_hist, [0, 180], 1)
+            ret, trackWindow = cv2.CamShift(dst, trackWindow, termination)
+            pts = cv2.boxPoints(ret)
+            pts = np.int0(pts)
+            cv2.polylines(frame, [pts], True, (0, 255, 0), 2)
+
+        cv2.imshow('frame', frame)
+
+        k = cv2.waitKey(60) & 0xFF
+        if k == 27:
+            break
+
+        if k == ord('i'):
+            print ('Select a tracking area and press any key')
+            inputmode = True
+            frame2 = frame.copy()
+
+            while inputmode:
+                cv2.imshow('frame', frame)
+                cv2.waitKey(0)
+
+    cap.release()
+    cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
     # showImage()
@@ -466,4 +740,17 @@ if __name__ == "__main__":
     # color_tracking()
     # thresholding()
     # bluring_filtering()
-    bluring()
+    # bluring()
+    # moment()
+    # convex()
+    # contour()
+    # contour_adv()
+    # histogram()
+    # equalization()
+    # hist2D()
+    # backProjection()
+    # fourier()
+    # tmpMatching()
+    # hough()
+    # watershed()
+    camShift()
