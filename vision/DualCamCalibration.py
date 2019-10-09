b8f38e054819b0c88e1a5af89004d9528ad5730e
hwangmh <gkgk1215@gmail.com>
2019-10-09 11:57:01 -0700

    dual camera calibration updated

--- vision/DualCamCalibration.py
+++ vision/DualCamCalibration.py
@@ -4,3 +4,97 @@ from sensor_msgs.msg import Image, CompressedImage
 from cv_bridge import CvBridge, CvBridgeError
 import threading
 import numpy as np
+import pyrealsense2 as rs
+
+class ImageConverter():
+    def __init__(self):
+        # data members
+        self.__bridge = CvBridge()
+
+        self.__img_raw_cam1 = []
+        self.__img_raw_cam2 = []
+
+        # Realsense configuring depth and color streams
+        self.pipeline = rs.pipeline()
+        self.config = rs.config()
+        self.config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+        # Realsense streaming start
+        self.pipeline.start(self.config)
+
+        # threading
+        t1 = threading.Thread(target=self.__run, args=(lambda: self.__stop_flag,))  # mainthread
+        t2 = threading.Thread(target=self.__img_raw_cam1_th)    # img receiving thread
+        t1.daemon = True
+        t2.daemon = True
+        self.__interval_ms = 10
+        self.__stop_flag = False
+
+        # ROS subscriber
+        self.__sub = rospy.Subscriber('/kinect2/hd/image_color/compressed', CompressedImage, self.__img_raw_cam2_cb)
+
+        # create ROS node
+        if not rospy.get_node_uri():
+            rospy.init_node('Image_pipeline_node', anonymous=True, log_level=rospy.WARN)
+            self.rate = rospy.Rate(1000.0 / self.__interval_ms)
+        else:
+            rospy.logdebug(rospy.get_caller_id() + ' -> ROS already initialized')
+
+        # start threading
+        t1.start()
+        t2.start()
+        rospy.spin()
+
+    def stop(self):
+        self.__stop_flag = True
+
+    def __run(self, stop):
+        while True:
+            if self.__img_raw_cam1==[] or self.__img_raw_cam2==[]:
+                pass
+            else:
+                # Resizing images
+                img1 = cv2.resize(self.__img_raw_cam1, (640, 480))  # MainCam (Realsense)
+                img2 = cv2.resize(self.__img_raw_cam2, (640, 360))  # AuxCam (Kinect2)
+
+                # Stack both images horizontally
+                # images = np.hstack((img1, img2))
+
+                cv2.imshow("Main cam", img1)
+                cv2.imshow("Aux cam", img2)
+
+                self.rate.sleep()
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    cv2.destroyAllWindows()
+                    stop()
+                    break
+
+    def __img_raw_cam1_th(self):
+        try:
+            while True:
+                # Wait for a coherent pair of frames: depth and color
+                frames = self.pipeline.wait_for_frames()
+                color_frame = frames.get_color_frame()
+
+                # Convert images to numpy arrays
+                self.__img_raw_cam1 = np.asanyarray(color_frame.get_data())
+
+        finally:
+            # Stop streaming
+            self.pipeline.stop()
+
+    def __img_raw_cam2_cb(self, data):
+        try:
+            if type(data).__name__ == 'CompressedImage':
+                self.__img_raw_cam2 = self.__compressedimg2cv2(data)
+            elif type(data).__name__ == 'Image':
+                self.__img_raw_cam2 = self.__bridge.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as e:
+            print(e)
+
+    def __compressedimg2cv2(self, comp_data):
+        np_arr = np.fromstring(comp_data.data, np.uint8)
+        return cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+
+if __name__ == '__main__':
+    ic = ImageConverter()
